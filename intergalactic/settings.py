3ab07dd08266eaccc6c3e596e4db97c07c0eefca
Juanjo Bazán <jjbazan@gmail.com>
2019-01-28 16:00:51 +0100

    add default settings

---
+++ intergalactic/settings.py
@@ -0,0 +1,15 @@
+"""Default settings and initial parameters
+All the values set here can be overwritten by the user in the input file: params.json
+"""
+
+default = {
+    "z": 0.02,
+    "lim_yields": "GAV",
+    "massive_yields": "CLI",
+    "sol_ab": "ag89",
+    "fe_coef": 1,
+    "imf": 2.35,
+    "imf_mup": 40.0,
+    "alpha_bin_stars": 0.05,
+    "sn_ia_selection": 3
+}
