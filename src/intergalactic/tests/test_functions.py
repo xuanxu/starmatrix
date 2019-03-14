743b09d1d6f9a63fb8ef318aa66ae247a35c48a1
Juanjo Bazán <jjbazan@gmail.com>
2019-03-14 11:51:48 +0100

    add coverage for functions

--- src/intergalactic/tests/test_functions.py
+++ src/intergalactic/tests/test_functions.py
@@ -1,5 +1,8 @@
 import pytest
+import numpy as np
 import intergalactic.functions as functions
+import intergalactic.constants as constants
+import intergalactic.settings as settings
 from intergalactic.imfs import Chabrier, Ferrini, Salpeter, Kroupa, MillerScalo, Maschberger, Starburst
 from intergalactic.abundances import AndersGrevesse1989, GrevesseSauval1998, Asplund2005, Asplund2009, Heger2010
 
@@ -23,3 +26,44 @@ def test_select_abundances():
 def test_secondary_mass_fraction():
     for m in [0.33, 3.33, 33.7, 73.0]:
         assert functions.secondary_mass_fraction(m) == 24 * m ** 2
+
+def test_mean_lifetime_stellar_mass_relation():
+    z                 = 0.02
+    stellar_mass_test = 4.0
+    lifetime_test     = 0.15
+
+    stellar_mass      = functions.stellar_mass(lifetime_test, z)
+    lifetime          = functions.mean_lifetime(stellar_mass_test, z)
+
+    assert np.isclose(functions.stellar_mass(lifetime, z), stellar_mass_test,  rtol = 0.05)
+    assert np.isclose(functions.mean_lifetime(stellar_mass, z), lifetime_test, rtol = 0.05)
+
+def test_value_in_interval():
+    interval_min  = 1.0
+    interval_max  = 100
+    interval      = [interval_min, interval_max]
+    value_in      = 25
+    value_out_min = 0.8
+    value_out_max = 101
+
+    assert functions.value_in_interval(value_in, interval) == value_in
+    assert functions.value_in_interval(value_out_min, interval) == interval_min
+    assert functions.value_in_interval(value_out_max, interval) == interval_max
+
+def test_no_negative_time_values():
+    t = -1
+    assert functions.supernovas_a_rate(t) == 0.0
+    assert functions.supernovas_b_rate(t) == 0.0
+    assert functions.total_energy_ejected(t) == 0.0
+    assert functions.sn_rate_ruiz_lapuente(t) == 0.0
+
+def test_imf_plus_primaries():
+    m_in_binaries_range = 5.0
+    m = constants.B_MIN - 0.5
+    imf = functions.select_imf(np.random.choice(settings.valid_values["imf"]))
+
+    assert functions.imf_plus_primaries(m, imf) == imf.for_mass(m) + functions.imf_binary_primary(m, imf)
+
+    imf_1 = imf.for_mass(m_in_binaries_range) * (1.0 - constants.BIN_FRACTION)
+    imf_2 = functions.imf_binary_primary(m_in_binaries_range, imf)
+    assert functions.imf_plus_primaries(m_in_binaries_range, imf) == imf_1 + imf_2
