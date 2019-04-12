fc026f833ed20e83b8348ad49cff3bb928866839
Juanjo Bazán <jjbazan@gmail.com>
2019-04-12 10:53:38 +0200

    increase mass range in imf tests

--- src/intergalactic/tests/test_imfs.py
+++ src/intergalactic/tests/test_imfs.py
@@ -30,6 +30,9 @@ def test_imf_is_zero_if_no_positive_mass(available_imfs):
         assert select_imf(imf).for_mass(0) == 0.0
         assert select_imf(imf).for_mass(-10) == 0.0
 
+        for mass in [0.02, 0.2, 0.75, 2, 8, 35, 90]:
+            assert select_imf(imf).for_mass(mass) > 0.0
+
 def test_for_mass_is_normalized(available_imfs):
     for imf_name in available_imfs:
         imf = select_imf(imf_name)
